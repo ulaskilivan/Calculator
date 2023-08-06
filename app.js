8ec9009a4763c69080696253fdb12c3f5ddb8510
Ulaş <ulas.klvn@gmail.com>
2023-08-06 13:37:52 +0300

    Calculator's Screen Rules Update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var screen=document.getElementById("screen");
 var validKeyboard=['0','1','2','3','4','5','6','7','8','9','/','*','+','-'];
+var validOperands=['x','/','+','-','*','=','.'];
 function clearScreen(){
     screen.innerText="";
 }
 function operandToScreen(op){
-    screen.insertAdjacentText("beforeend",op);
+    if(!validOperands.includes(screen.innerText.slice(-1))||screen.innerText.slice(-1)===""){
+        screen.insertAdjacentText("beforeend",op);
+    }
 }
 function numberToScreen(num){
     screen.insertAdjacentText("beforeend",num);
@@ -13,6 +16,11 @@ function numberToScreen(num){
  document.addEventListener("keyup",keyboardListener);
  function keyboardListener(key){
     if(validKeyboard.includes(key.key)){
-        screen.insertAdjacentText("beforeend",key.key);
+        if(!validOperands.includes(key.key)||!validOperands.includes(screen.innerText.slice(-1))){
+            screen.insertAdjacentText("beforeend",key.key);
+        }
     }
+    else if(key.key==='Backspace'){
+        screen.innerText=screen.innerText.slice(0,-1);
+     }
 }
